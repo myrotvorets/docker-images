af3c1dabb9c6f1a2dd4e06160905d2844c795a46
Myrotvorets <support@myrotvorets.center>
2020-09-15 03:55:31 +0300

    Initial import

---
+++ cfssl/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+[ -f /run/secrets/cfssl_authkey ]  || { echo "/run/secrets/cfssl_authkey is required but not found"; exit 1; }
+[ -f /run/secrets/mysql_password ] || { echo "/run/secrets/mysql_password is required but not found"; exit 1; }
+[ -f /run/secrets/ca_int_key ]     || { echo "/run/secrets/ca_int_key is required but not found"; exit 1; }
+[ -f /srv/cfssl/cfssl.json.in ]    || { echo "/srv/cfssl/cfssl.json.in is required but not found"; exit 1; }
+[ -f /srv/cfssl/dbconfig.json.in ] || { echo "/srv/cfssl/dbconfig.json.in is required but not found"; exit 1; }
+[ -f /srv/cfssl/ca-int.pem ]       || { echo "/srv/cfssl/ca-int.pem is required but not found"; exit 1; }
+[ -f /srv/cfssl/ca.pem ]           || { echo "/srv/cfssl/ca.pem is required but not found"; exit 1; }
+
+AUTHKEY="$(cat /run/secrets/cfssl_authkey)"
+MYSQL_PASSWORD="$(cat /run/secrets/mysql_password)"
+
+sed "s/{{AUTHKEY}}/$AUTHKEY/" /srv/cfssl/cfssl.json.in > /srv/cfssl/cfssl.json
+sed "s/{{MYSQL_USER}}/$MYSQL_USER/; s/{{MYSQL_DATABASE}}/$MYSQL_DATABASE/; s/{{MYSQL_HOST}}/$MYSQL_HOST/; s/{{MYSQL_PASSWORD}}/$MYSQL_PASSWORD/" /srv/cfssl/dbconfig.json.in > /srv/cfssl/dbconfig.json
+
+CFSSL_LISTEN_ADDRESS=${CFSSL_LISTEN_ADDRESS:-0.0.0.0}
+CFSSL_LISTEN_PORT=${CFSSL_LISTEN_PORT:-8888}
+
+exec cfssl serve \
+    -address=$CFSSL_LISTEN_ADDRESS \
+    -port=$CFSSL_LISTEN_PORT \
+    -ca=/srv/cfssl/ca-int.pem \
+    -ca-key=/run/secrets/ca_int_key \
+    -ca-bundle=/srv/cfssl/ca.pem \
+    -int-bundle=/srv/cfssl/ca-int.pem \
+    -tls-remote-ca=/srv/cfssl/ca-int.pem \
+    -config=/srv/cfssl/cfssl.json \
+    -db-config=/srv/cfssl/dbconfig.json \
+    -disable init_ca,newcert
---
+++ tinc/entrypoint.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+if [ ! -d "/etc/tinc/${NETWORK_NAME}" ]; then
+    if [ -z "${NETWORK_ADDRESS}" ]; then
+        echo NETWORK_ADDRESS environment variable is required
+        exit 1
+    fi
+
+    if [ -n "${SERVER}" ]; then
+        /usr/sbin/tinc -n "${NETWORK_NAME}" init "${NODE_NAME:-$(hostname)}"
+        cat > "/etc/tinc/${NETWORK_NAME}/invitation-created" <<'EOF'
+#!/bin/sh
+
+cat > $INVITATION_FILE <<EOT
+Name = $NODE
+Netname = $NETNAME
+ConnectTo = $NAME
+#----------------#
+EOT
+/usr/sbin/tinc export-all >> $INVITATION_FILE
+EOF
+        chmod +x "/etc/tinc/${NETWORK_NAME}/invitation-created"
+    elif [ -n "${INVITE_URL}" ]; then
+        /usr/sbin/tinc join "${INVITE_URL}"
+    fi
+
+    /usr/sbin/tinc -n "${NETWORK_NAME}" add subnet ${NETWORK_ADDRESS}
+    if [ -d "/etc/tinc/${NETWORK_NAME}" ]; then
+        cat > "/etc/tinc/${NETWORK_NAME}/tinc-up" <<EOF
+#!/bin/sh
+
+/sbin/ip addr add ${NETWORK_ADDRESS}/${NETWORK_PREFIX:-24} dev \$INTERFACE
+/sbin/ip link set \$INTERFACE up
+EOF
+
+        cat > "/etc/tinc/${NETWORK_NAME}/tinc-down" <<EOF
+#!/bin/sh
+
+/sbin/ip link set \$INTERFACE down
+/sbin/ip addr del ${NETWORK_ADDRESS}/${NETWORK_PREFIX:-24} dev \$INTERFACE
+EOF
+
+        chmod +x "/etc/tinc/${NETWORK_NAME}/tinc-up" "/etc/tinc/${NETWORK_NAME}/tinc-down"
+    fi
+fi
+
+exec /usr/sbin/tinc $@

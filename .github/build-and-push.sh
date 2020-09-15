11b72e34a4eb5af0fdfbe56dca3ad59a90b682c6
Myrotvorets <support@myrotvorets.center>
2020-09-15 04:06:46 +0300

    Correct arguments to 'docker build'

--- .github/build-and-push.sh
+++ .github/build-and-push.sh
@@ -3,5 +3,5 @@
 set -e
 
 for i in cfssl node node-build node-current node-current-min node-min tinc; do
-    (cd "$i" && docker build -t "myrotvorets/$i:latest" && docker push "myrotvorets/$i:latest")
+    (cd "$i" && docker build -t "myrotvorets/$i:latest" . && docker push "myrotvorets/$i:latest")
 done

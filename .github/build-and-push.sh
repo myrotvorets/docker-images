cebf180e96db5f1bb9425aecf825975a6ad1edbc
Myrotvorets <support@myrotvorets.center>
2020-10-30 00:20:22 +0200

    Add node-current-build

--- .github/build-and-push.sh
+++ .github/build-and-push.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-for i in cfssl node node-build node-current node-current-min node-min tinc; do
+for i in cfssl node node-build node-current node-current-build node-current-min node-min tinc; do
     (cd "$i" && docker build -t "myrotvorets/$i:latest" . && docker push "myrotvorets/$i:latest")
 done

78f50db9189fe581e3b9a80e32e06834403f8f6c
Myrotvorets <support@myrotvorets.center>
2021-04-17 15:53:22 +0300

    Add trivy

--- .github/build.sh
+++ .github/build.sh
@@ -5,8 +5,9 @@ set -x
 
 TAG=${GITHUB_SHA:-$(date +%s)}
 
+touch built_images.txt
 for i in */; do
     if [ -f "${i}Dockerfile" ]; then
-        (cd "${i}" && docker build --pull -t "myrotvorets/${i%/}:latest" -t "${i%/}:${TAG}" .)
+        (cd "${i}" && docker build --pull -t "myrotvorets/${i%/}:latest" -t "${i%/}:${TAG}" .) && echo "${i%/}:${TAG}" >> built_images.txt
     fi
 done

ff9f78b8a927318766c23c4e55da709fcba84cfe
Myrotvorets <support@myrotvorets.center>
2021-04-17 15:06:39 +0300

    Bug fix

--- .github/build.sh
+++ .github/build.sh
@@ -7,6 +7,6 @@ TAG=${GITHUB_SHA:-$(date +%s)}
 
 for i in */; do
     if [ -f "${i}Dockerfile" ]; then
-        (cd "${i}" && docker build --pull -t "myrotvorets/${i#/}:latest" -t "${i#/}:${TAG}" .)
+        (cd "${i}" && docker build --pull -t "myrotvorets/${i%/}:latest" -t "${i%/}:${TAG}" .)
     fi
 done

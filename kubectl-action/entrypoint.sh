fca6937481d43637f840c2ee1be9389bf130aae7
Myrotvorets <support@myrotvorets.center>
2021-12-10 15:26:23 +0200

    Update packages and image in kubectl-action
    * Bump Alpine to 3.15;
    * Bump kubectl1.18 to 1.18.20;
    * Bump kubectl1.20 to 1.20.13;
    * Bump kubectl1.21 to 1.21.7;
    * Bump kubectl1.22 to 1.22.4;
    * Add kubectl1.23 (1.23.0);
    * Update entrypoint.sh

--- kubectl-action/entrypoint.sh
+++ kubectl-action/entrypoint.sh
@@ -6,19 +6,19 @@ echo "${KUBE_CONFIG_DATA:-}" | base64 -d > /tmp/kubeconfig
 export KUBECONFIG=/tmp/kubeconfig
 
 case "${KUBECTL_VERSION:=latest}" in
-    1.1[89] | 1.2[012])
-        KUBECTL=kubectl${KUBECTL_VERSION}
-    ;;
-
     latest)
         KUBECTL=kubectl
     ;;
 
     *)
-        echo "::warning::unknown kubectl version requested: ${KUBECTL_VERSION}"
-        KUBECTL=kubectl
+        KUBECTL=kubectl${KUBECTL_VERSION}
     ;;
 esac
+
+if [ ! -x "${KUBECTL}" ]; then
+    echo "::warning::unknown kubectl version requested: ${KUBECTL_VERSION}"
+    KUBECTL=kubectl
+fi
 
 trap 'cleanup' 0 HUP INT QUIT ABRT TERM
 
